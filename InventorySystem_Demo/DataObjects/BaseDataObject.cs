28ab3607998eb9721e42a8eda342b2e0e41e9b3f
ElmoZhong <947390399@qq.com>
2016-09-28 09:37:33 +0800

    Create DataObjects Project

---
+++ InventorySystem_Demo/DataObjects/BaseDataObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataObjects
+{
+    public class BaseDataObject
+    {
+        public string Description { get; set; }
+        public int StatusCode { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public int CreatedBy { get; set; }
+
+        /*Join*/
+        public string CreatedName { get; set; }
+    }
+}
