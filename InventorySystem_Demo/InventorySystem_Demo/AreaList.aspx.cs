4437eb8018fddfe22dbd3aa32dfb621ee4a7da65
何蕴 <2238883436@qq.cm>
2016-10-10 17:34:11 +0800

    new object

--- InventorySystem_Demo/InventorySystem_Demo/AreaList.aspx.cs
+++ InventorySystem_Demo/InventorySystem_Demo/AreaList.aspx.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Edward.DAL;
+using Edward.BLL;
+using Edward.DBHelper;
+using Edward.Framework;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace InventorySystem_Demo
 {
@@ -11,7 +17,51 @@ namespace InventorySystem_Demo
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                Bind(); 
+            }
+        }
+        public void Bind()
+        {
+            string sql = "select * from TF_Area where StatusCode=1";
+            DataTable dt = BaseDAL.DBHelper.GetList(sql);
+            GridView1.DataSource = dt;
+            GridView1.DataKeyNames = new string[] { "AreaId" };
+            GridView1.DataBind();
+            GetNull(dt);
+        }
 
+        public void GetNull(DataTable dt)
+        {
+            int columnCount = dt.Columns.Count;
+            //if (columnCount==0)
+            //{
+                GridView1.Rows[0].Cells.Clear();
+                GridView1.Rows[0].Cells.Add(new TableCell());
+                GridView1.Rows[0].Cells[0].ColumnSpan = columnCount;
+                GridView1.Rows[0].Cells[0].Text = "没有记录";
+                GridView1.Rows[0].Cells[0].Style.Add("text-align", "center");
+                GridView1.Rows[0].Cells[0].Style.Add("color", "red"); 
+            //}
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            GridViewRow gvr = (GridViewRow)((LinkButton)(e.CommandSource)).Parent.Parent;//获取父本实例化
+            int ID = gvr.RowIndex;//获取行的ID;
+            int AID = Convert.ToInt32(GridView1.DataKeys[ID].Value);//获取区域ID
+            if (e.CommandName== "Profile")
+            {
+                Session["AID"] = AID;
+                Response.Redirect("~/AreaProfile.aspx");
+            }
+            if (e.CommandName=="MyDelete")
+            {
+                SqlParameter[] p = new SqlParameter[] { new SqlParameter("@id", AID) };
+                BaseDAL.DBHelper.Update("update TF_Area set StatusCode=2 where AreaId=@id", p);
+                Bind();
+            }
         }
     }
 }
--- InventorySystem_Demo/InventorySystem_Demo/AreaProfile.aspx
+++ InventorySystem_Demo/InventorySystem_Demo/AreaProfile.aspx
@@ -10,7 +10,38 @@
 <body>
     <form id="form1" runat="server">
     <div>
-    
+        <div>
+            <asp:Button ID="btnSubmt" runat="server" OnClick="btnSubmt_Click" Text="修改" />
+            <asp:Button ID="btnCancel" runat="server" OnClick="btnCancel_Click" Text="取消" />
+        </div>
+        <div>
+            <ul>
+                <li>
+                    <div>区域编号：</div><div><asp:TextBox ID="txtCode" runat="server"></asp:TextBox></div><div></div>
+                </li>
+                <li>
+                    <div>区域名称：</div><div><asp:TextBox ID="txtName" runat="server"></asp:TextBox></div><div></div>
+                </li>
+                <li>
+                    <div>区域等级：</div><div><asp:TextBox ID="txtLevel" runat="server"></asp:TextBox></div><div></div>
+                </li>
+                <li>
+                    <div>区域负责人：</div><div><asp:TextBox ID="txtOwner" runat="server"></asp:TextBox></div><div></div>
+                </li>
+                <li>
+                    <div>创建人：</div><div><asp:TextBox ID="txtCreatedBy" runat="server"></asp:TextBox></div><div></div>
+                </li>
+                <li>
+                    <div>创建时间：</div><div><asp:TextBox ID="txtCreatedTime" runat="server"></asp:TextBox></div><div></div>
+                </li>
+                <li>
+                    <div>状态：</div><div><asp:TextBox ID="txtStatusCode" runat="server"></asp:TextBox></div><div></div>
+                </li>
+                <li>
+                    <div>描述：</div><div><asp:TextBox ID="txtDescription" runat="server" TextMode="MultiLine"></asp:TextBox></div><div></div>
+                </li>
+            </ul>
+        </div>
     </div>
     </form>
 </body>
